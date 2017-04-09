efdc2bd251ba5b5debc5130f710abc20e329ad34
henry <henry@bestapp.us>
2017-04-09 14:44:05 +0800

    新增字符串处理相关的工具类

--- app/src/main/java/com/taro/base/utils/StringUtil.java
+++ app/src/main/java/com/taro/base/utils/StringUtil.java
@@ -1,7 +1,6 @@
 package com.taro.base.utils;
 
-import android.graphics.Point;
-import android.support.annotation.NonNull;
+import java.util.List;
 
 /**
  * Created by taro on 16/12/23.
@@ -20,32 +19,74 @@ public class StringUtil {
     }
 
     /**
-     * 解析图片路径后的分辨率,默认格式为 _widthxheight
+     * 将数组的对象以连接符合并为一个字符串
      *
-     * @param outPoint 用于输出width及height的对象
-     * @param baseSize
-     * @param url      图片路径, ...._200x200.jpg 类型
+     * @param arr
+     * @param connectChar        连接符
+     * @param isSkipNull         当元素为null时是否跳过合并
+     * @param placeHolderForNull 当元素为null时使用统一的替换字符(当isSkipNull为false时有效)
+     * @return
      */
-    public static void getImageWidthHeightFromResolution(@NonNull Point outPoint, int baseSize, String url) {
-        if (url == null || !url.contains("x")) {
-            outPoint.set(0, 0);
-            return;
-        }
-        String resolution = url.substring(url.lastIndexOf('_') + 1, url.lastIndexOf('.'));
-        String[] params = resolution.split("x");
-        float width = Float.valueOf(params[0]);
-        float height = Float.valueOf(params[1]);
-        float value = width / height;
-        if (value > 1) {
-            //TODO:根据需要再去将宽高及其比例计算出相关的数据
-            //宽大于高
-//            width = baseSize * 2;
-//            height = width / value;
+    public static String joinStr(String[] arr, String connectChar, boolean isSkipNull, String placeHolderForNull) {
+        if (arr != null && arr.length > 0) {
+            Object[] obj = new Object[arr.length];
+            System.arraycopy(arr, 0, obj, 0, arr.length);
+            return joinStr(obj, connectChar, isSkipNull, placeHolderForNull);
         } else {
-//            height = baseSize * 2;
-//            width = height * value;
+            return null;
         }
-        outPoint.set((int) width, (int) height);
+    }
+
+    /**
+     * 将数组的对象以连接符合并为一个字符串
+     *
+     * @param arr
+     * @param connectChar        连接符
+     * @param isSkipNull         当元素为null时是否跳过合并
+     * @param placeHolderForNull 当元素为null时使用统一的替换字符(当isSkipNull为false时有效)
+     * @return
+     */
+    public static String joinStr(Object[] arr, String connectChar, boolean isSkipNull, String placeHolderForNull) {
+        if (arr != null) {
+            if (connectChar == null) {
+                connectChar = "";
+            }
+            StringBuilder builder = new StringBuilder(arr.length * 10);
+            for (Object obj : arr) {
+                if (obj != null) {
+                    builder.append(obj.toString());
+                    builder.append(connectChar);
+                } else if (!isSkipNull) {
+                    builder.append(placeHolderForNull);
+                    builder.append(connectChar);
+                }
+            }
+            //删除最后的连接符
+            if (builder.length() > 0) {
+                builder.delete(builder.length() - connectChar.length(), builder.length());
+            }
+            return builder.toString();
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * 将列表的对象以连接符合并为一个字符串
+     *
+     * @param list
+     * @param connectChar        连接符
+     * @param isSkipNull         当元素为null时是否跳过合并
+     * @param placeHolderForNull 当元素为null时使用统一的替换字符(当isSkipNull为false时有效)
+     * @return
+     */
+    public static String joinStr(List<? extends Object> list, String connectChar, boolean isSkipNull, String placeHolderForNull) {
+        if (list != null) {
+            Object[] obj = list.toArray();
+            return joinStr(obj, connectChar, isSkipNull, placeHolderForNull);
+        } else {
+            return null;
+        }
     }
 
     /**
@@ -54,7 +95,7 @@ public class StringUtil {
      * @param bmpPath
      * @return
      */
-    public static String getBmpSuffix(String bmpPath) {
+    public static String getFileSuffix(String bmpPath) {
         if (bmpPath == null) {
             return "";
         } else {
